3a2bfaad2bb6e91c94996e91c03bc670d841740e
Doğan Çeçen <sepeth@gmail.com>
2011-08-28 16:39:01 +0300

    added IPC between daemon and cli

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='mgurses',
+    version='0.1dev',
+    packages=['mgurses'],
+    license='GPL-3',
+    long_description=open('README.md').read()
+)
